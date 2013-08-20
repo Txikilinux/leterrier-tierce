6e29f8976862333c2323a97d4c5e348d9a6062fc
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2013-08-20 15:53:30 +0200

    Modification version.h

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,0,5,0
-#define VER_FILEVERSION_STR         "1.0.5\0"
+#define VER_FILEVERSION             1,4,0,0
+#define VER_FILEVERSION_STR         "1.4.0\0"
 
-#define VER_PRODUCTVERSION          1,0,5,0
-#define VER_PRODUCTVERSION_STR      "1.0.5\0"
+#define VER_PRODUCTVERSION          1,4,0,0
+#define VER_PRODUCTVERSION_STR      "1.4.0\0"
 
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
 #define VER_FILEDESCRIPTION_STR     "AbulEdu LeTerrier -- Tierce"
