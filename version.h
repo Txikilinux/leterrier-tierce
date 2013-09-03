bc2dba3b568ce66e15e48eddad878f3b2235c6f2
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2013-09-03 17:29:01 +0200

    Passage du numéro de version en 2.0

--- version.h
+++ version.h
@@ -24,16 +24,16 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,4,0,0
-#define VER_FILEVERSION_STR         "1.4.0\0"
+#define VER_FILEVERSION             2,0,0,0
+#define VER_FILEVERSION_STR         "2.0.0\0"
 
-#define VER_PRODUCTVERSION          1,4,0,0
-#define VER_PRODUCTVERSION_STR      "1.4.0\0"
+#define VER_PRODUCTVERSION          2,0,0,0
+#define VER_PRODUCTVERSION_STR      "2.0.0\0"
 
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
 #define VER_FILEDESCRIPTION_STR     "AbulEdu LeTerrier -- Tierce"
 #define VER_INTERNALNAME_STR        "leterrier-tierce"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2012 RyXeo SARL"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2013 RyXeo SARL"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    "License GNU/GPL v3"
 #define VER_ORIGINALFILENAME_STR    "leterrier-tierce.exe"
