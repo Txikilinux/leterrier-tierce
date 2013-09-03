69b817680c09e14974ad94c66fcc73c4af9a3108
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2013-09-03 10:19:38 +0200

    En-têtes

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 /**
-  * Tierce (le terrier d'AbulEdu)
+  * Tierce (le Terrier d'AbulÉdu)
   *
   * @warning aucun traitement d'erreur n'est pour l'instant implémenté
-  * @see https://redmine.ryxeo.com/projects/
-  * @author 2011 Andre Connes <andre dot connes at wanadoo dot fr>
+  * @see https://redmine.ryxeo.com/projects/leterrier-fubuki
+  * @author 2011 André Connes <andre.connes@wanadoo.fr>
+  * @author 2011-2012 Eric Seigne <eric.seigne@ryxeo.com>
   * @see The GNU Public License (GPL)
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation; either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+  * for more details.
+  *
+  * You should have received a copy of the GNU General Public License along
+  * with this program; if not, write to the Free Software Foundation, Inc.,
+  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
   */
-
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
 
 
 #include "version.h"
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,35 +1,33 @@
 /**
-  * Tierce (le terrier d'AbulEdu)
+  * Tierce (le Terrier d'AbulÉdu)
   *
   * @warning aucun traitement d'erreur n'est pour l'instant implémenté
-  * @see https://redmine.ryxeo.com/projects/
-  * @author 2011 Andre Connes <andre dot connes at wanadoo dot fr>
-  * @author 2011 Eric Seigne <eric.seigne@ryxeo.com> (qtranslator)
+  * @see https://redmine.ryxeo.com/projects/leterrier-fubuki
+  * @author 2011 André Connes <andre.connes@wanadoo.fr>
+  * @author 2011-2012 Eric Seigne <eric.seigne@ryxeo.com>
+  * @author 2012-2013 Philippe Cadaugade <philippe.cadaugade@ryxeo.com>
+  * @author 2012-2013 Icham Sirat <icham.sirat@ryxeo.com>
   * @see The GNU Public License (GPL)
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation; either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+  * for more details.
+  *
+  * You should have received a copy of the GNU General Public License along
+  * with this program; if not, write to the Free Software Foundation, Inc.,
+  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
   */
-
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
-
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "btncase.h"
 #include <QTimer>
-
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
