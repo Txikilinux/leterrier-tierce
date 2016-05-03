4ac63785d348a90130e579c749450a906c0ca9d7
Icham Sirat <icham.sirat@ryxeo.com>
2016-05-03 11:38:54 +0200

    Passage en version 2.0.2

--- src/version.h
+++ src/version.h
@@ -24,11 +24,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             2,0,1,0
-#define VER_FILEVERSION_STR         "2.0.1\0"
+#define VER_FILEVERSION             2,0,2,0
+#define VER_FILEVERSION_STR         "2.0.2\0"
 
-#define VER_PRODUCTVERSION          2,0,1,0
-#define VER_PRODUCTVERSION_STR      "2.0.1\0"
+#define VER_PRODUCTVERSION          2,0,2,0
+#define VER_PRODUCTVERSION_STR      "2.0.2\0"
 
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
 #define VER_FILEDESCRIPTION_STR     "AbulEdu LeTerrier -- Tierce"
